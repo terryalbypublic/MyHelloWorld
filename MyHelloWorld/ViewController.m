37474ea84a256c3ac3742cf9107a5e7b2f7ae24b
Terence Alberti <terence.alberti@xmedia.ch>
2015-12-01 15:01:21 +0100

    my stuff

--- MyHelloWorld/ViewController.m
+++ MyHelloWorld/ViewController.m
@@ -17,11 +17,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appWillResignActive:) name:UIApplicationWillResignActiveNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appWillTerminate:) name:UIApplicationWillTerminateNotification object:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+-(void)appWillResignActive:(NSNotification*)note
+{
+    self.view.backgroundColor = [UIColor redColor];
+    
+}
+-(void)appWillTerminate:(NSNotification*)note
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationWillResignActiveNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationWillTerminateNotification object:nil];
+    
+}
+
 
 @end
